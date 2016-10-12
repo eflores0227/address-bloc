b52f61a0e2b8e81e4687d58042c4afea2585282b
Estefania Flores <eflores0227@gmail.com>
2016-10-11 20:12:34 -0700

    assignemnt 2, without txt answers

---
+++ assignment_2.rb
@@ -0,0 +1,11 @@
+def snake_camel(snake_str)
+  string = snake_str.gsub(/(_[a-z])/) {|match| match[1].upcase}
+end
+
+def find_by(attribute, value)
+  answer = connection.execute <<- SQL
+    SELECT #{attribute} FROM #{table}
+    WHERE value = #{value}
+  SQL
+  answer
+end
