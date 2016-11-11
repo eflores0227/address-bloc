53f0fa944d0cc916b5546f7d2d3380fd48eb2e4c
Estefania Flores <eflores0227@gmail.com>
2016-11-11 12:14:34 -0800

    checkpoint and assignment 5

--- models/entry.rb
+++ models/entry.rb
@@ -5,4 +5,15 @@ class Entry < BlocRecord::Base
   def to_s
     "Name: #{name}\nPhone Number: #{phone_number}\nEmail: #{email}"
   end
+
+  def self.method_missing(method, *args, &block)
+    value_and_parameter = ""
+    value = ""
+    parameter = ""
+    method.to_s
+    method.match(/^find_by_(.*)$/) { |match| value_and_parameter = match }
+    value_and_parameter.match(/^\w*/) { |match| value = match }
+    value_and_parameter.match(/"(\w*)/) { |match| parameter = match }
+    Entry.find_by(:value, parameter)
+  end
 end
